8ddb2a5362246cbe548a2882f21ae8c3cbc01908
Vadym Hlushko <ver2ex@MacBook-Air-Vadym.local>
2022-10-27 20:09:36 +0300

    refactoring

--- middleware/imageMiddleware.js
+++ middleware/imageMiddleware.js
@@ -6,7 +6,7 @@ module.exports = function (req, res, next) {
     next();
   }
   try {
-    const token = req.headers.authorization.split(" ")[1];
+    const token = req.headers.authorization?.split(" ")[1];
     if (!token) {
       return res.status(403).json({ message: "Not authorizied" });
     }
