b52c9ad132c3958e3cc2e9a434209ab41efd36bd
Vadym Hlushko <ver2ex@MacBook-Air-Vadym.local>
2022-10-27 20:11:21 +0300

    refactoring

--- routers/imageRouter/index.js
+++ routers/imageRouter/index.js
@@ -6,10 +6,10 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../../uploads");
+    cb(null, "./uploads/");
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    cb(null, new Date().toISOString() + file.originalname);
   },
 });
 
